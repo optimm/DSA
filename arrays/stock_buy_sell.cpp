6d49b9ff691ea30e0426f932a525f34a7c0101ee
optimm <ayushsaxena823@gmail.com>
2022-02-05 20:51:55 +0530

    stocks buy sell recursive

--- arrays/stock_buy_sell.cpp
+++ arrays/stock_buy_sell.cpp
@@ -3,6 +3,25 @@
 using namespace std;
 /// naive method recursive  - refere to notebook
 // very great implementation in the problem
+int maxProfit(int price[], int start, int end)
+{
+    if (end <= start)
+        return 0;
+
+    int profit = 0;
+    for (int i = start; i < end; i++)
+    {
+        for (int j = i + 1; j <= end; j++)
+        {
+            if (price[j] > price[i])
+            {
+                int curr_profit = price[j] - price[i] + maxProfit(price, start, i - 1) + maxProfit(price, j + 1, end);
+                profit = max(profit, curr_profit);
+            }
+        }
+    }
+    return profit;
+}
 int stocks(vector<int> a)
 {
     int n = a.size();
