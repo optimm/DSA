be5486fc0f402f68894b98dfdfcc0f75b36a9674
optimm <ayushsaxena823@gmail.com>
2022-02-07 01:25:27 +0530

    three sorted common

---
+++ arrays/.cph/common_three_sorted.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+vector<int> commonElements(int A[], int B[], int C[], int n1, int n2, int n3)
+{
+    int i = 0, j = 0, k = 0;
+
+    vector<int> res;
+    int last = INT_MIN;
+    while (i < n1 and j < n2 and k < n3)
+    {
+        if (A[i] == B[j] and A[i] == C[k] and A[i] != last)
+        {
+            res.push_back(A[i]);
+            last = A[i];
+            i++;
+            j++;
+            k++;
+        }
+        else if (min({A[i], B[j], C[k]}) == A[i])
+            i++;
+        else if (min({A[i], B[j], C[k]}) == B[j])
+            j++;
+        else
+            k++;
+    }
+    return res;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    return 0;
+}
